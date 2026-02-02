d0061635cac062880ce7826a691cdc5f8725bb5b
ltf <1035869369@qq.com>
2026-02-03 02:06:03 +0800

    UI/UX Pro Max: 全新现代化设计

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Astro Blog';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const SITE_TITLE = '小龙虾的博客';
+export const SITE_DESCRIPTION = '探索技术、分享见解、记录成长 - 小龙虾的个人博客';
